6655f425833295a6a21c0e45b6908a39e3e55ec3
RobsonMCambraia <robson.mcambraia23@gmail.com>
2024-03-07 17:04:11 -0300

    LER E APRESENTAR A IMAGEM

--- detector.py
+++ detector.py
@@ -4,9 +4,13 @@ import numpy as np
 import time
 
 capture = cv2.VideoCapture("src\hand_01.mp4")
+width, height = 740, 580
 
 while True:
-    _, img = capture.read()
+    ret, img = capture.read()
+    
+    img = cv2.resize(img, (width, height))
+    
     cv2.imshow("hands", img)
     
     if cv2.waitKey(20) & 0xFF == ord('q') or not ret:
