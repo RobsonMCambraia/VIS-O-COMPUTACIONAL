bec4603a20f20932137a906ab19a4971a14e74cf
RobsonMCambraia <robson.mcambraia23@gmail.com>
2024-03-07 16:58:20 -0300

    Inicio do projeto de visão computacional

---
+++ detector.py
@@ -0,0 +1,13 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+
+capture = cv2.VideoCapture("src\hand_01.mp4")
+
+while True:
+    _, img = capture.read()
+    cv2.imshow("hands", img)
+    
+    if cv2.waitKey(20) & 0FF==ord('q'):
+        break
