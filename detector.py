1f99a5d14c5421258d5f2a67926e0cd9c87a4152
RobsonMCambraia <robson.mcambraia23@gmail.com>
2024-03-07 17:17:54 -0300

    criação da classe de detecção

--- detector.py
+++ detector.py
@@ -3,15 +3,29 @@ import mediapipe as mp
 import numpy as np
 import time
 
-capture = cv2.VideoCapture("src\hand_01.mp4")
-width, height = 740, 580
+class handsDetector:
+    def __init__(self, 
+                 mode: bool = False,
+                 n_hands: int = 2,
+                 complexity: int = 1,
+                 min_detection: float = 0.5,
+                 min_traking: float = 0.5):
+        self.mode = mode
+        self.n_hands = n_hands
+        self.complexity = complexity
+        self.min_detection = min_detection
+        self.min_traking = min_traking
 
-while True:
-    ret, img = capture.read()
-    
-    img = cv2.resize(img, (width, height))
-    
-    cv2.imshow("hands", img)
-    
-    if cv2.waitKey(20) & 0xFF == ord('q') or not ret:
-        break
+if __name__ == '__main__':
+    capture = cv2.VideoCapture("src\hand_01.mp4")
+    width, height = 740, 580
+
+    while True:
+        ret, img = capture.read()
+        
+        img = cv2.resize(img, (width, height))
+        
+        cv2.imshow("hands", img)
+        
+        if cv2.waitKey(20) & 0xFF == ord('q') or not ret:
+            break
