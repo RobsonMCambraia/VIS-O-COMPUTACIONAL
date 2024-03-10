fb7efd51bbc089029b4de8af103fef283fad7bbc
RobsonMCambraia <robson.mcambraia23@gmail.com>
2024-03-10 15:27:41 -0300

    correção de bugs

--- detector.py
+++ detector.py
@@ -1,31 +1,29 @@
 import cv2
-import mediapipe as mp
 import numpy as np
-import time
-from keras.models import load_model  # TensorFlow is required for Keras to work
-
+from keras.models import load_model
+import mediapipe as mp
 class HandsDetector:
-    def __init__(self, 
+    def __init__(self,
                  mode: bool = False,
                  n_hands: int = 2,
                  complexity: int = 1,
                  min_detection: float = 0.5,
-                 min_traking: float = 0.5):
-        
+                 min_tracking: float = 0.5):
         self.mode = mode
         self.n_hands = n_hands
         self.complexity = complexity
         self.min_detection = min_detection
-        self.min_traking = min_traking
-        
+        self.min_tracking = min_tracking
+
         self.pipe_hands = mp.solutions.hands
-        self.hands = self.pipe_hands.Hands(self.mode, 
-                                          self.n_hands, 
-                                          self.complexity, 
-                                          self.min_detection, 
-                                          self.min_traking)
-        
+        self.hands = self.pipe_hands.Hands(self.mode,
+                                           self.n_hands,
+                                           self.complexity,
+                                           self.min_detection,
+                                           self.min_tracking)
+
         self.pipe_draw = mp.solutions.drawing_utils
+        self.x_max, self.y_max, self.x_min, self.y_min = 0, 0, 0, 0
 
     def mp_hands(self, img: np.ndarray, draw_hands: bool = True):
         rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -37,27 +35,23 @@ class HandsDetector:
                     self.pipe_draw.draw_landmarks(
                         img, hand_landmarks, self.pipe_hands.HAND_CONNECTIONS)
         return img
-    
-    def encontrar_dedos(self,
-                        img: np.ndarray,
-                        num_hand: int = 0,):
+
+    def encontrar_dedos(self, img: np.ndarray, num_hand: int = 0):
         self.requerid_landmark_list = []
-        
+
         if self.result.multi_hand_landmarks:
-            my_hand = self.result.multi_hand_landmarks[0]
-            
+            my_hand = self.result.multi_hand_landmarks[num_hand]
+
             for id, landmark in enumerate(my_hand.landmark):
                 height, width, _ = img.shape
-                center_x = int(landmark.x*width)
-                center_y = int(landmark.y*height)
-                
+                center_x = int(landmark.x * width)
+                center_y = int(landmark.y * height)
+
                 self.requerid_landmark_list.append([id, center_x, center_y])
-                
+
         return self.requerid_landmark_list
-    
-    def desenhar_info_dedo(self, 
-                           img: np.ndarray,
-                           info_dedo):
+
+    def desenhar_info_dedo(self, img: np.ndarray, info_dedo):
         fonte = cv2.FONT_HERSHEY_SIMPLEX
         cor_texto = (255, 255, 255)
         tamanho_fonte = 0.5
@@ -66,15 +60,12 @@ class HandsDetector:
         dedo, posicao_x, posicao_y = map(int, info_dedo)
 
         texto = f'Dedo: {dedo}, X: {posicao_x}, Y: {posicao_y}'
-        cv2.putText(img, texto, (posicao_x, posicao_y - 10), fonte, tamanho_fonte, cor_texto, espessura_linha, cv2.LINE_AA)
-        
+        cv2.putText(img, texto, (posicao_x, posicao_y - 10), fonte, tamanho_fonte, cor_texto, espessura_linha,
+                    cv2.LINE_AA)
+
     def desenhar_box(self, img: np.ndarray):
-        height, width, _ = img.shape  #posição da mão
+        height, width, _ = img.shape  # posição da mão
 
-        self.x_max, self.y_max = 0, 0
-        self.x_min, self.y_min = width, height
-
-        
         if self.result.multi_hand_landmarks:
             for hand in self.result.multi_hand_landmarks:
                 self.x_max, self.y_max = float('-inf'), float('-inf')
@@ -87,33 +78,33 @@ class HandsDetector:
                     self.y_max = y if y > self.y_max else self.y_max
                     self.y_min = y if y < self.y_min else self.y_min
 
-                return cv2.rectangle(img, (self.x_min - 50, self.y_min - 50), (self.x_max + 50, self.y_max + 50), (0, 255, 0), 2)
+                cv2.rectangle(img, (self.x_min - 50, self.y_min - 50), (self.x_max + 50, self.y_max + 50), (0, 255, 0), 2)
 
     def deteccao_modelo(self, img: np.ndarray, data, model, classes):
         try:
             # Corta a mão da imagem
             img_corte = img[self.y_min - 50: self.y_max + 50, self.x_min - 50: self.x_max + 50]
-            
+
             # Redimensiona para o tamanho esperado pelo modelo
             img_corte = cv2.resize(img_corte, (224, 224))
-            
+
             # Normaliza a imagem
             normalized = (img_corte.astype(np.float32) / 255.0)
-            
+
             # Adiciona uma dimensão extra para criar um lote
             data[0] = np.expand_dims(normalized, axis=0)
-            
+
             # Realiza a predição
             prediction = model.predict(data)
-            
-            # Obtém o índice da classe prevista
-            indexVal = np.argmax(prediction)
-            
-            # Adiciona a classe prevista à imagem
-            cv2.putText(img, classes[indexVal], (self.x_min - 50, self.y_min - 65), cv2.FONT_HERSHEY_COMPLEX, 3, (0, 0, 255), 5)
-            
-            return img
-        
-        except Exception as e:
-            return (f"Erro na detecção do modelo: {e}")
 
+            # Obtém o índice da classe prevista
+            index_val = np.argmax(prediction)
+
+            # Adiciona a classe prevista à imagem
+            cv2.putText(img, classes[index_val], (self.x_min - 50, self.y_min - 65), cv2.FONT_HERSHEY_COMPLEX, 3,
+                        (0, 0, 255), 5)
+
+            return img
+
+        except Exception as e:
+            return f"Erro na detecção do modelo: {e}"
--- main.py
+++ main.py
@@ -1,39 +1,27 @@
 import cv2
-import mediapipe as mp
 import numpy as np
-import time
-from keras.models import load_model  # TensorFlow is required for Keras to work
-
-from detector import HandsDetector
+import mediapipe as mp
+from keras.models import load_model
+from detector import HandsDetector 
 
 capture = cv2.VideoCapture("src\hand_01.mp4")
 # capture = cv2.VideoCapture(0)
-width, height = 740, 580
 
 Detector = HandsDetector()
 
 # Modelo keras
-classes = ['A','B','C','D','E', 'F', 'G']
+classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 model = load_model("model\keras_model.h5")
-data = np.ndarray(shape=(1, 244, 244, 3), dtype=np.float32) # array de entrada
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)  # array de entrada
 classes = [line.strip() for line in open("model\labels.txt", "r")]
-
 
 while True:
     ret, img = capture.read()
 
-    # img = cv2.resize(img, (width, height))
     Detector.mp_hands(img, draw_hands=True)
     landmark_hand = Detector.encontrar_dedos(img)
-    box_hand = Detector.desenhar_box(img)
-    model_detection = Detector.deteccao_modelo(img, data, model, classes)
-     
-    # escreve na tela as coordenadas do dedo informado
-    # if landmark_hand:
-    #     for dedo_info in landmark_hand:
-    #         dedo, _, _ = dedo_info
-    #         if dedo in (2, 8): 
-    #             Detector.desenhar_info_dedo(img, dedo_info)        
+    Detector.desenhar_box(img)
+    Detector.deteccao_modelo(img, data, model, classes)
 
     cv2.imshow("hands", img)
 
