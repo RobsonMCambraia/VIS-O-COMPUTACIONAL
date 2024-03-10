4b05820bff84de115165b996c54319b5f48f8fde
RobsonMCambraia <robson.mcambraia23@gmail.com>
2024-03-10 13:17:43 -0300

    importação do modelo keras

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import time
+from keras.models import load_model  # TensorFlow is required for Keras to work
+
 from detector import HandsDetector
 
 capture = cv2.VideoCapture("src\hand_01.mp4")
@@ -9,6 +11,12 @@ capture = cv2.VideoCapture("src\hand_01.mp4")
 width, height = 740, 580
 
 Detector = HandsDetector()
+
+# Modelo
+model = load_model("model\keras_model.h5")
+data = np.ndarray(shape=(1, 244, 244, 3), dtype=np.float32)
+class_names = open("model\labels.txt", "r").readlines()
+
 
 while True:
     ret, img = capture.read()
